880a4d7e37b8d74a0db5c8ab553e605f1e3d2ae4
Suyash Sharma <sharmasuyash821+cc@gmail.com>
2025-02-23 16:58:14 +0530

    chore: setup shadcn and next themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import RootProviders from "@/components/providers/RootProviders";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={`${inter.className} antialiased`}>{children}</body>
+      <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+        <body className={`${inter.className} antialiased`}>
+          <RootProviders>{children}</RootProviders>
+        </body>
       </html>
     </ClerkProvider>
   );
