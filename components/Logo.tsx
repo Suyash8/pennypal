b6724613dce202258dfb55c87601e93abdd2a4e5
Suyash Sharma <sharmasuyash821+cc@gmail.com>
2025-02-23 23:09:17 +0530

    feat(dashboard): navbar improvements
    added the sign in button when the user is not signed in
    added handle resize to close sidebar when width hits md breakpoint

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -13,4 +13,14 @@ function Logo() {
   );
 }
 
+export function MobileLogo() {
+  return (
+    <Link href="/" className="flex items-center gap-2">
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold tracking-tighter leading-tight text-transparent">
+        PennyPal
+      </p>
+    </Link>
+  );
+}
+
 export default Logo;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,9 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import Logo from "@/components/Logo";
+import Logo, { MobileLogo } from "@/components/Logo";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import { SignInButton, useAuth, UserButton } from "@clerk/nextjs";
 import { ThemeSwitcherButton } from "@/components/ThemeSwitcherButton";
 import {
   Sheet,
@@ -31,19 +31,57 @@ const items = [
   { label: "Manage", link: "/manage" },
 ];
 
+function SignIn() {
+  return (
+    <Button variant="outline" asChild>
+      <SignInButton />
+    </Button>
+  );
+}
+
+function User() {
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      className="rounded-full hover:bg-transparent"
+    >
+      <UserButton />
+    </Button>
+  );
+}
+
+function AuthButton() {
+  const { userId } = useAuth();
+  return <>{userId ? <User /> : <SignIn />}</>;
+}
+
 function MobileNavbar() {
   const [isOpen, setIsOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup on component unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="flex items-center justify-center border-separate border-b bg-background md:hidden">
       <nav className="container flex items-center justify-between px-4">
         <Sheet open={isOpen} onOpenChange={setIsOpen}>
           <SheetTrigger asChild>
-            <Button variant="ghost" size="icon">
+            <Button variant="outline" size="icon">
               <Menu />
             </Button>
           </SheetTrigger>
-          <SheetContent className="w-[400px] sm:w-[540px]" side="left">
+          <SheetContent className="w-[min(400px,100vw)]" side="left">
             <SheetTitle className="sr-only">Menu</SheetTitle>
             <Logo />
             <div className="flex flex-col gap-1 pt-4">
@@ -59,11 +97,11 @@ function MobileNavbar() {
           </SheetContent>
         </Sheet>
         <div className="flex h-[80px] hin-h-[60px] items-center gap-x-4">
-          <Logo />
+          <MobileLogo />
         </div>
         <div className="flex items-center gap-2">
           <ThemeSwitcherButton />
-          <UserButton />
+          <AuthButton />
         </div>
       </nav>
     </div>
@@ -88,7 +126,7 @@ function DesktopNavbar() {
         </div>
         <div className="flex items-center gap-2">
           <ThemeSwitcherButton />
-          <UserButton />
+          <AuthButton />
         </div>
       </nav>
     </div>
